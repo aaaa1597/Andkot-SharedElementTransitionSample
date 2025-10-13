50bd5e899c89a96cccc9fdf2ef8403b105edb5af
aaaa1597 <tokushi2011@gmail.com>
2025-10-13 14:06:24 +0900

    WIP: Implement shared element transition (not functioning yet). 共有要素遷移を実装したけど動作せず版。

--- app/src/main/java/com/aaa/sharedelementtransitionsample/MainFragment.kt
+++ app/src/main/java/com/aaa/sharedelementtransitionsample/MainFragment.kt
@@ -1,6 +1,7 @@
 package com.aaa.sharedelementtransitionsample
 
 import android.os.Bundle
+import android.transition.TransitionInflater
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -23,8 +24,15 @@ class MainFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        /* 共有要素トランジション設定 */
+        sharedElementReturnTransition = TransitionInflater.from(requireContext())
+            .inflateTransition(android.R.transition.move)
+
         binding.imageView1.setOnClickListener {
-            ImagesSheetDialogFragment().show(parentFragmentManager, "ImagesSheetDialogFragment")
+            val dialog = ImagesSheetDialogFragment()
+            dialog.sharedElementReturnTransition = TransitionInflater.from(requireContext())
+                .inflateTransition(android.R.transition.move)
+            dialog.showWithSharedElement(requireActivity().supportFragmentManager, binding.imageView1)
         }
 
         binding.imageView2.setOnClickListener {
